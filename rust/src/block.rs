85795b927bd9d54e1bd6fe262377335454d858d1
Julie Dujardin <julie@dujardin.pw>
2024-07-20 17:59:14 +0200

    piece control: rotate, move

--- rust/src/block.rs
+++ rust/src/block.rs
@@ -5,19 +5,32 @@ use godot::prelude::*;
 #[class(base=Area2D)]
 pub struct Block {
     base: Base<Area2D>,
+    pub board_offset: Vector2,
 }
 
 #[godot_api]
 impl IArea2D for Block {
     fn init(base: Base<Area2D>) -> Self {
-        Block { base }
+        Block {
+            board_offset: Vector2::ZERO,
+            base,
+        }
+    }
+
+    fn ready(&mut self) {
+        self.update_position();
     }
 }
 
 #[godot_api]
 impl Block {
+    pub fn update_position(&mut self) {
+        let position = self.board_offset * self.get_size();
+        self.base_mut().set_position(position);
+    }
+
     #[func]
-    pub fn get_size(&self) -> Vector2 {
+    fn get_size(&self) -> Vector2 {
         self.base()
             .get_node_as::<CollisionShape2D>("CollisionShape2D")
             .get_shape()
--- rust/src/piece.rs
+++ rust/src/piece.rs
@@ -21,6 +21,8 @@ pub struct Piece {
     #[export]
     shape: Shape,
     blocks: VariantArray,
+    block_size: Vector2,
+    center_block_position: Vector2,
 
     shape_blocks: Dictionary,
     shape_bounds: Dictionary,
@@ -30,22 +32,29 @@ pub struct Piece {
 
 #[godot_api]
 impl Piece {
-    fn mov(&mut self, direction: Vector2) {}
+    fn mov(&mut self, direction: Vector2) {
+        let curr_position = self.base().get_position();
+        let block_size = self.block_size;
+        self.base_mut().set_position(
+            Vector2::new(
+                match direction {
+                    Vector2::LEFT => -1.,
+                    _ => 1.,
+                } * block_size.x,
+                0.,
+            ) + curr_position,
+        )
+    }
 
     fn drop(&mut self) {}
 
     fn rotate(&mut self) {
-        let start = varray![Vector2::new(0., -1.), Vector2::new(0., 0.), Vector2::new(0., 1.), Vector2::new(1., 1.)];
-        let rotate = varray![Vector2::new(1., 0.), Vector2::new(0., 0.), Vector2::new(-1., 0.), Vector2::new(-1., 1.)];
-
-        for (i, block_offset) in self
-            .shape_blocks
-            .get(self.shape.to_godot())
-            .unwrap()
-            .to::<VariantArray>()
-            .iter_shared().enumerate()
-        {
-            let block = self.blocks.at(i);
+        for block in self.blocks.iter_shared() {
+            let mut block_ref = block.to::<Gd<Block>>();
+            let mut block = block_ref.bind_mut();
+            let new_cell = Vector2::new(-block.board_offset.y, block.board_offset.x);
+            block.board_offset = new_cell;
+            block.update_position();
         }
     }
 }
@@ -56,6 +65,8 @@ impl INode2D for Piece {
         Piece {
             blocks: varray![],
             shape: Shape::O, // TODO override this after instantiation but before add_child
+            center_block_position: Vector2::new(5., 2.),
+            block_size: Vector2::ZERO, // Gets set in self.ready()
             shape_blocks: dict![
                 // Shapes rotate around their (0, 0) block
                 "I": varray![Vector2::new(-1., 0.), Vector2::new(0., 0.), Vector2::new(1., 0.), Vector2::new(2., 0.)],
@@ -91,12 +102,14 @@ impl INode2D for Piece {
             .iter_shared()
         {
             let mut block = block_scene.instantiate_as::<Block>();
+            self.block_size = block.call("get_size".into(), &[]).to::<Vector2>();
+            let piece_position = self.center_block_position * self.block_size;
+            self.base_mut().set_position(piece_position);
 
-            let block_size = block.call("get_size".into(), &[]).to::<Vector2>();
-            let block_position =
-                block_offset.to::<Vector2>() * block_size + Vector2::new(200., 200.);
-            godot_print!("{}", block_position);
-            block.set_position(block_position);
+            {
+                let mut block_ref = block.bind_mut();
+                block_ref.board_offset = block_offset.to::<Vector2>();
+            }
 
             self.blocks.push(block.to_variant());
             self.base_mut().add_child(block.clone().upcast());
