ae427e2db7ac06776aeb1a45f96c7b68640d9679
Julie Dujardin <julie@dujardin.pw>
2024-07-21 22:00:55 +0200

    handle piece select success

--- rust/src/select.rs
+++ rust/src/select.rs
@@ -1,27 +1,27 @@
 use crate::piece::{Piece, Shape};
-use godot::classes::InputEvent;
-use godot::engine::Sprite2D;
+use godot::classes::{InputEvent, Sprite2D};
 use godot::prelude::*;
-use std::f32::consts::PI;
 use rand::prelude::IndexedRandom;
+use std::f32::consts::PI;
 
 const SELECT_COUNT: usize = 3;
 
 #[derive(Clone, Copy, PartialEq)]
 enum InputOptions {
-    UP,
-    LEFT,
-    RIGHT,
+    Up,
+    Left,
+    Right,
 }
 
-const INPUT_CHOICES: [InputOptions; 3] = [InputOptions::UP, InputOptions::LEFT, InputOptions::RIGHT];
+const INPUT_CHOICES: [InputOptions; 3] =
+    [InputOptions::Up, InputOptions::Left, InputOptions::Right];
 
 #[derive(GodotClass)]
 #[class(base=Node2D)]
 pub struct Select {
     piece: Gd<Piece>,
     sequence: Vec<InputOptions>,
-    select_options: Vec<Gd<Sprite2D>>,
+    prompts: Vec<Gd<Sprite2D>>,
     curr_check_index: usize,
 
     base: Base<Node2D>,
@@ -30,13 +30,42 @@ pub struct Select {
 #[godot_api]
 impl Select {
     fn check_input(&mut self, input: InputOptions) {
-        if input == self.sequence[self.curr_check_index] {}
+        if input == self.sequence[self.curr_check_index] {
+            self.prompts[self.curr_check_index]
+                .set_modulate(Color::from_rgb(1., 1., 1.));
+            self.curr_check_index += 1;
+            if self.curr_check_index >= SELECT_COUNT {
+                self.success();
+            }
+        }
+        else {
+            self.reset();
+        }
+    }
+
+    fn reset(&mut self) {
+        if self.curr_check_index > 0 {
+            self.curr_check_index = 0;
+            for mut prompt in &mut self.prompts {
+                prompt.set_modulate(Color::from_rgb(105./255., 105./255., 105./255.));
+            }
+        }
+    }
+
+    fn success(&mut self) {
+        self.reset();
+        // TODO move piece
+        self.piece.clone().free();
+
+        self.sequence = Select::random_sequence();
+        self.piece = Piece::spawn_random();
+        self.ready();
     }
 
     fn random_sequence() -> Vec<InputOptions> {
         let mut rng = rand::thread_rng();
 
-        (0..SELECT_COUNT).into_iter()
+        (0..SELECT_COUNT)
             .map(|_| *INPUT_CHOICES.choose(&mut rng).unwrap())
             .collect()
     }
@@ -48,7 +77,7 @@ impl INode2D for Select {
         Select {
             piece: Piece::spawn_random(),
             sequence: Select::random_sequence(),
-            select_options: vec![],
+            prompts: vec![],
             curr_check_index: 0,
             base,
         }
@@ -61,12 +90,12 @@ impl INode2D for Select {
                 .get_node_as::<Sprite2D>(format!("SelectOption{}", i.to_string().as_str()));
 
             match expected_input {
-                InputOptions::UP => select_node.set_rotation(-PI / 2.),
-                InputOptions::LEFT => select_node.set_rotation(-PI),
-                InputOptions::RIGHT => select_node.set_rotation(0.),
+                InputOptions::Up => select_node.set_rotation(-PI / 2.),
+                InputOptions::Left => select_node.set_rotation(-PI),
+                InputOptions::Right => select_node.set_rotation(0.),
             }
 
-            self.select_options.push(select_node);
+            self.prompts.push(select_node);
         }
 
         {
@@ -80,28 +109,34 @@ impl INode2D for Select {
                 piece_bind.shape.clone()
             };
             match shape {
-                Shape::I => {self.piece.set_position(Vector2::new(42., 22.));},
-                Shape::T => {self.piece.set_position(Vector2::new(42., 22.));},
+                Shape::I => {
+                    self.piece.set_position(Vector2::new(42., 22.));
+                }
+                Shape::T => {
+                    self.piece.set_position(Vector2::new(42., 22.));
+                }
                 Shape::J => {
-                    self.piece.set_rotation(PI/2.);
+                    self.piece.set_rotation(PI / 2.);
                     self.piece.set_position(Vector2::new(62., 22.));
-                },
+                }
                 Shape::L => {
-                    self.piece.set_rotation(PI/2.);
+                    self.piece.set_rotation(PI / 2.);
                     self.piece.set_position(Vector2::new(62., 2.));
-                },
-                _ => {},
+                }
+                _ => {}
             }
         }
     }
 
     fn input(&mut self, event: Gd<InputEvent>) {
         if event.is_action_pressed("up".into()) {
-            self.check_input(InputOptions::UP);
+            self.check_input(InputOptions::Up);
         } else if event.is_action_pressed("left".into()) {
-            self.check_input(InputOptions::LEFT);
+            self.check_input(InputOptions::Left);
         } else if event.is_action_pressed("right".into()) {
-            self.check_input(InputOptions::RIGHT);
+            self.check_input(InputOptions::Right);
+        } else if event.is_action_pressed("down".into()) {
+            self.reset();
         }
     }
 }
